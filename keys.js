2a33a1416abc7ef77003a80753e0033bd0028ec1
Prachi <prachi.waghmare92@gmail.com>
2024-09-07 23:31:50 +0530

    Minor changes to application

--- keys.js
+++ keys.js
@@ -1,4 +1,6 @@
 module.exports = {
-    mongoUrl : "mongodb+srv://prachi11:11Prachi!!@cluster0.iga0pes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    Jwt_secret : "dsdfijsdfijj"
+  //mongoUrl : "mongodb+srv://prachi11:11Prachi!!@cluster0.iga0pes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  mongoUrl:
+    "mongodb+srv://prachi:prachiuser@atlascluster.kjhnk.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster",
+  Jwt_secret: "dsdfijsdfijj",
 };
